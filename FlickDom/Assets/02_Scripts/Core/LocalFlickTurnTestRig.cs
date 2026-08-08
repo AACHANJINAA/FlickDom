77c756649099ed4ed1c14290d2266a36cc91e226
tlsdn0403.home_DeskTop <148981969+tlsdn0403@users.noreply.github.com>
2026-08-08 15:54:47 +0900

    스타트 게임할 때 플레이 화면 건드리는 문제 해결

--- FlickDom/Assets/02_Scripts/Core/LocalFlickTurnTestRig.cs
+++ FlickDom/Assets/02_Scripts/Core/LocalFlickTurnTestRig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using FlickDom.Networking;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
@@ -69,6 +70,8 @@ namespace FlickDom.Gameplay
         private Coroutine noPlacementAdvanceRoutine;
         private Canvas orderLabelCanvas;
         private readonly List<Text> orderLabels = new List<Text>(3);
+        private bool suppressPieceOrderInputUntilPointerReleased;
+        private int pieceOrderInputUnlockFrame;
 
         private void Awake()
         {
@@ -338,6 +341,11 @@ namespace FlickDom.Gameplay
 
             Mouse mouse = Mouse.current;
             if (mouse == null || inputCamera == null || !mouse.leftButton.wasPressedThisFrame)
+            {
+                return;
+            }
+
+            if (ShouldSuppressPieceOrderInput(mouse))
             {
                 return;
             }
@@ -1120,6 +1128,7 @@ namespace FlickDom.Gameplay
             if (nextState == FlickDomGameState.PieceOrderSelection)
             {
                 StopPendingNoPlacementAdvance();
+                SuppressPieceOrderInputUntilPointerReleased();
                 CompleteOrderSelectionIfNoPieces();
             }
             else if (nextState == FlickDomGameState.PlayerFlicking)
@@ -1138,6 +1147,34 @@ namespace FlickDom.Gameplay
 
             RefreshPieceHighlights();
             RefreshOrderLabels();
+        }
+
+        private void SuppressPieceOrderInputUntilPointerReleased()
+        {
+            suppressPieceOrderInputUntilPointerReleased = true;
+            pieceOrderInputUnlockFrame = Time.frameCount + 1;
+        }
+
+        private bool ShouldSuppressPieceOrderInput(Mouse mouse)
+        {
+            EventSystem eventSystem = EventSystem.current;
+            if (eventSystem != null && eventSystem.IsPointerOverGameObject())
+            {
+                return true;
+            }
+
+            if (!suppressPieceOrderInputUntilPointerReleased)
+            {
+                return false;
+            }
+
+            if (mouse.leftButton.isPressed || Time.frameCount <= pieceOrderInputUnlockFrame)
+            {
+                return true;
+            }
+
+            suppressPieceOrderInputUntilPointerReleased = false;
+            return false;
         }
 
         private void BeginNoPlacementAdvanceIfNeeded()
